dbda8ea9a83932db16bcf2801202ce75671be840
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 21:59:23 -0500

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-gray-100 py-20">
+      <div className="container mx-auto text-center">
+        <h2 className="text-4xl font-bold mb-4">Bienvenido a JG Assistants</h2>
+        <p className="text-lg text-gray-600">
+          Soluciones de asistencia virtual personalizadas.
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
