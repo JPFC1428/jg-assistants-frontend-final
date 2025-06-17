eb5d2865e426f2f906a59fded4dd4ed2e25139be
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 22:01:19 -0500

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-blue-600 p-4 text-center text-white">
+      <p>&copy; 2024 JG Assistants. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
