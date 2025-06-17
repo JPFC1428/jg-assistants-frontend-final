f6436f2780fe515ed31dcce57c58f5b09c1b310b
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 21:58:36 -0500

    Create Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-white font-bold text-xl">JG Assistants</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
