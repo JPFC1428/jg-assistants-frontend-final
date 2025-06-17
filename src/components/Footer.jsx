9ee1ce12647d75aed10630c75e1957fc0b4650be
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 22:22:31 -0500

    Update and rename src/Footer.jsx to src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-blue-600 p-4 text-center text-white">
-      <p>&copy; 2024 JG Assistants. Todos los derechos reservados.</p>
+      <p>&copy; 2025 JG Assistants. Todos los derechos reservados.</p>
     </footer>
   );
 }
