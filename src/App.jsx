64f2caf76727c84edcd6ade8b04d192688959498
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 22:44:34 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import Features from './components/Features';
+import Services from './components/Services';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Header />
-      <Hero />
-      <Features />
+      <Services />
       <Footer />
-    </div>
+    </>
   );
 }
 
