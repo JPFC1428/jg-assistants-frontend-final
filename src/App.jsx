652b8cd403ead07eaa14a2db72314ffa92001d31
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 22:02:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import Services from './Services';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="app">
-      <h1>JG Assistants Frontend funcionando</h1>
+    <div>
+      <Navbar />
+      <Header />
+      <Services />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
