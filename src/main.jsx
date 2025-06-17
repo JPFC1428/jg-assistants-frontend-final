2810df47812446addb03bc6155e23064a3ccec13
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-16 22:45:38 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// ✅ Importamos Bootstrap y FontAwesome
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// También tu index.css si tienes estilos propios (opcional)
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
